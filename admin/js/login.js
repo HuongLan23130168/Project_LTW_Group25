3a55879d2323fa6821b796991f0b0f42d61528cf
Hương Lan <23130168@st.hcmuaf.edu.vn>
2025-11-12 00:19:11 +0700

    add login,main js

---
+++ admin/js/login.js
@@ -0,0 +1,36 @@
+// Xóa log trong console mỗi khi tải lại page
+console.clear();
+
+// Chờ nội dung HTML tải xong -> chạy script
+document.addEventListener("DOMContentLoaded", () => {
+  // Lấy 2 form: signup và signin
+  const signup = document.querySelector(".signup");
+  const signin = document.querySelector(".signin");
+
+  const signinBtn = document.getElementById("signin");
+  const signupBtn = document.getElementById("signup");
+
+  // === CHUYỂN GIỮA 2 FORM ===
+  // Khi nhấn "Đăng nhập" → ẩn signup, hiện signin
+  signinBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    signup.classList.add("slide-up");
+    signin.classList.remove("slide-up");
+  });
+
+  // Khi nhấn "Đăng ký" → ẩn signin, hiện signup
+  signupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    signin.classList.add("slide-up");
+    signup.classList.remove("slide-up");
+  });
+
+  // === NÚT ĐĂNG KÝ ===
+  document
+    .querySelector(".signup .submit-btn")
+    .addEventListener("click", (e) => {
+      e.preventDefault();
+      signup.classList.add("slide-up");
+      signin.classList.remove("slide-up");
+    });
+});
