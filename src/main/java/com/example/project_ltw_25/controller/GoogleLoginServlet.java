8668ac6f683ee493d0febf5652ba0fc96aec958c
Hương Lan <23130168@st.hcmuaf.edu.vn>
2026-05-29 23:54:59 +0700

    vd2

--- src/main/java/com/example/project_ltw_25/controller/GoogleLoginServlet.java
+++ src/main/java/com/example/project_ltw_25/controller/GoogleLoginServlet.java
@@ -1,39 +1,20 @@
-package com.example.project_ltw_25.controller;
+package com.example.ttltw_project.controller.user;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import com.example.ttltw_project.config.GoogleOAuthConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.ResourceBundle;
 
-
-@WebServlet(name = "GoogleLoginServlet", value = "/google-login")
+@WebServlet(name = "GoogleLoginServlet", value = "/login-google")
 public class GoogleLoginServlet extends HttpServlet {
-    private static final ResourceBundle rb =
-            ResourceBundle.getBundle("application");
-
-    private static final String CLIENT_ID =
-            rb.getString("google.client_id");
-
-    private static final String REDIRECT_URI =
-            rb.getString("google.redirect_uri");
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String loginUrl = "https://accounts.google.com/o/oauth2/v2/auth"
-                + "?client_id=" + CLIENT_ID
-                + "&redirect_uri=" + URLEncoder.encode(REDIRECT_URI, StandardCharsets.UTF_8)
-                + "&response_type=code"
-                + "&scope=email profile";
-
-        response.sendRedirect(loginUrl);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String redirectUrl = GoogleOAuthConfig.getAuthorizationUrl();
+        response.sendRedirect(redirectUrl);
     }
 }
